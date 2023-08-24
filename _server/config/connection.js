184a8a365635920d83d6e45be676a97a0a2136e8
debabrata-pw08-429 <chintuthecoder@gmail.com>
2023-08-24 21:52:30 +0530

    Removed Server files

--- _server/config/connection.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize("blog_db", "root", "tC]@2ZK5rz,J~lT", {
-  host: "localhost",
-  dialect: "mysql",
-});
-
-module.exports = sequelize;
